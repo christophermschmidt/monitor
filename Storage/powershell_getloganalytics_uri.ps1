eba23ef2a8d67915c0ef996068db60fcbe6b0cb3
christophermschmidt <46455343+christophermschmidt@users.noreply.github.com>
2021-06-22 09:45:27 -0400

    added data lake and storage sample queries

---
+++ Storage/powershell_getloganalytics_uri.ps1
@@ -0,0 +1,19 @@
+﻿#import-module Az
+
+#login to azure
+Connect-AzAccount
+
+Install-Module Az.OperationalInsights
+
+$resourceGroupName = "<theNameOfYourResourceGroup>"
+$workspaceName = "<theNameOfTheLogAnalyticsWorkspace>"
+
+#$uri = "https://portal.loganalytics.io/subscriptions/{0}/resourcegroups/{1}/workspaces/{2}" -f $subscriptionId, $resourceGroupName, $workspaceName
+
+#instead of the uri, get the guid for the power bi report instead:
+$workspace = Get-AzOperationalInsightsWorkspace -ResourceGroupName $resourceGroupName -Name $workspaceName
+$GuidString = $workspace.CustomerId.Guid.ToString()
+$pbistring = "https://api.loganalytics.io/v1/workspaces/$GuidString/query"
+$pbistring
+
+
